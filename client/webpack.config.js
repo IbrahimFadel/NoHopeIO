89ee389397714a394ad7ff851d0f383c9a29b230
IbrahimFadel <ibrahim.m.fadel@gmail.com>
2019-11-27 17:57:40 -0500

    Add development option(only bundles changed files)

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,41 +1,44 @@
 var path = require('path');
-var pathToPhaser = path.join(__dirname, "/node_modules/phaser/");
-var phaser = path.join(pathToPhaser, "dist/phaser.js");
+var pathToPhaser = path.join(__dirname, '/node_modules/phaser/');
+var phaser = path.join(pathToPhaser, 'dist/phaser.js');
 var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
+// If dev is true, it executes faster, if you want to run production, set it to false(it will take longer but be smaller)
+const dev = false;
 
 module.exports = {
-    entry: "./src/index.ts",
-    output: {
-        path: path.resolve(__dirname, "../dist/public"),
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            { test: /\.ts$/, loader: "ts-loader", exclude: "/node_modules/" },
-            { test: /phaser\.js$/, loader: "expose-loader?Phaser" }
-        ]
-    },
-    resolve: {
-        extensions: [ '.tsx', '.ts', '.js' ],
-        alias: {
-            phaser: phaser
-        }
-    },
-    externals: {
-        // require("jquery") is external and available
-        //  on the global var jQuery
-        "phaser": "Phaser.min"
-    },
-    optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        uglifyOptions: {
-          output: {
-            comments: false
-          }
-        }
-      })
-    ]
-  }
+	entry: './src/index.ts',
+	output: {
+		path: path.resolve(__dirname, '../dist/public'),
+		filename: 'bundle.js',
+	},
+	mode: dev ? 'development' : 'production',
+	module: {
+		rules: [
+			{ test: /\.ts$/, loader: 'ts-loader', exclude: '/node_modules/' },
+			{ test: /phaser\.js$/, loader: 'expose-loader?Phaser' },
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+		alias: {
+			phaser: phaser,
+		},
+	},
+	externals: {
+		// require("jquery") is external and available
+		//  on the global var jQuery
+		phaser: 'Phaser.min',
+	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				uglifyOptions: {
+					output: {
+						comments: false,
+					},
+				},
+			}),
+		],
+	},
 };
